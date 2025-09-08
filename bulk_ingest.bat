64bae54e05606e9d700632f45665e42ae13df072
Alistair-Spencer <Alistair_Spencer@yahoo.com>
2025-09-07 18:57:57 -0700

    Create bulk_ingest.bat

---
+++ bulk_ingest.bat
@@ -0,0 +1,11 @@
+@echo off
+set "API=https://www.pranayai.com/ingest"
+echo Uploading PDFs, TXTs, and MDs from .\data\
+
+for %%f in (data\*.pdf data\*.txt data\*.md) do (
+  echo   Ingesting %%f ...
+  curl -s -X POST "%API%" -F "files=@%%f"
+)
+
+echo Done!
+pause
